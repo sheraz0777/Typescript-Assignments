fd9d84fd0407ee542989e70c58ab005cbb9d0bf2
sheraz0777 <m.sheraz0777@gmail.com>
2024-02-22 21:27:23 +0500

    Solve exercise till 02

---
+++ main.ts
@@ -0,0 +1,16 @@
+//Exercise No 01
+
+//Install Node.js, TypeScript and VS Code on your computer.
+
+//Exercise No 02
+
+//Personal Message: Store a person’s name in a variable, and print a message to that person. Your message should be simple, such as, “Hello Eric, would you like to learn some Python today?”
+
+let myName = "Hello Sheraz"
+let message = "would you like to learn some Python today?"
+
+console.log(myName, message);
+
+console.log("Hello Ramzan");
+console.log("Hello Hassan");
+console.log("Hello Shehraz");
